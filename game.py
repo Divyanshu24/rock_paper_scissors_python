8afc0ddf6d47adb55d6519c77fbadfc2f4acb47a
Kunal <kunalverma.learn@gmail.com>
2022-12-16 13:40:02 +0530

    final game

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ print("********************************************************************\nWel
 
 score = 0
 
-pl = input("\nEnter your choice\n1. Press '1' for Rock.\n2. Press '2' for Paper.\n3. Press '3' for Scissors.\nEnter  \"No\" for exiting\n ")
+pl = input("\nEnter your choice\n1. Press '1' for Rock.\n2. Press '2' for Paper.\n3. Press '3' for Scissors.\nEnter  \"No\" for exiting\n")
 
 if(pl[0] == 'n' or pl[0] == 'N'):
     app.ext(score)
@@ -22,7 +22,10 @@ while pl[0] != 'n' or pl[0] != 'N':
     os.system('cls')
     score += app.play(int(pl[0]))
     print(f"\nYour current score is {score} ")
-    pl = input("\nEnter your choice\n1. Press '1' for Rock.\n2. Press '2' for Paper.\n3. Press '3' for Scissors.\nEnter  \"No\" for exiting\n ")
+    pl = input("\nEnter your choice\n1. Press '1' for Rock.\n2. Press '2' for Paper.\n3. Press '3' for Scissors.\nEnter  \"No\" for exiting\n")
+    if(pl[0] == 'n' or pl[0] == 'N'):
+    app.ext(score)
+
 
 if(pl[0] == 'n' or pl[0] == 'N'):
     app.ext(score)
