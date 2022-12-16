ed38b9e9b93a91cd352a17391ae964505c4362bd
Kunal <kunalverma.learn@gmail.com>
2022-12-16 13:52:23 +0530

    double enter bug removed

--- game.py
+++ game.py
@@ -6,7 +6,9 @@ print("********************************************************************\nWel
 
 score = 0
 
-pl = input("\nEnter your choice\n1. Press '1' for Rock.\n2. Press '2' for Paper.\n3. Press '3' for Scissors.\nEnter  \"No\" for exiting\n")
+pl = ''
+while pl == '':
+    pl = input("\nEnter your choice\n1. Press '1' for Rock.\n2. Press '2' for Paper.\n3. Press '3' for Scissors.\nEnter  \"No\" for exiting\n")
 
 if(pl[0] == 'n' or pl[0] == 'N'):
     app.ext(score)
@@ -16,15 +18,22 @@ while pl[0] != 'n' or pl[0] != 'N':
     while (ord(pl[0]) > 51) or (ord(pl[0]) < 49):
         os.system('cls')
         print("\nWRONG CHOICE!!!!\n")
-        pl = input("\nEnter your choice\n1. Press '1' for Rock.\n2. Press '2' for Paper.\n3. Press '3' for Scissors.\nEnter \"No\" for exiting\n")
+        pl = ''
+        while pl == '':
+            pl = input("\nEnter your choice\n1. Press '1' for Rock.\n2. Press '2' for Paper.\n3. Press '3' for Scissors.\nEnter  \"No\" for exiting\n")
+
         if(pl[0] == 'n' or pl[0] == 'N'):
             app.ext(score)
     os.system('cls')
     score += app.play(int(pl[0]))
     print(f"\nYour current score is {score} ")
-    pl = input("\nEnter your choice\n1. Press '1' for Rock.\n2. Press '2' for Paper.\n3. Press '3' for Scissors.\nEnter  \"No\" for exiting\n")
+    
+    pl = ''
+    while pl == '':
+        pl = input("\nEnter your choice\n1. Press '1' for Rock.\n2. Press '2' for Paper.\n3. Press '3' for Scissors.\nEnter  \"No\" for exiting\n")
+
     if(pl[0] == 'n' or pl[0] == 'N'):
-    app.ext(score)
+        app.ext(score)
 
 
 if(pl[0] == 'n' or pl[0] == 'N'):
